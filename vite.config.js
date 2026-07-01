5f0d6a89678fa4cc973665cf0283265a6cec344f
mkt-medicalgroup <comunicazionemedicalgroup@gmail.com>
2026-07-01 16:52:07 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // IMPORTANTE: se pubblichi su GitHub Pages con un URL del tipo
 // https://tuonome.github.io/nome-repository/
-// cambia la riga "base" sotto in:  base: '/nome-repository/',
+// cambia la riga "base" sotto in:  base: '/magazzino-divise/',
 // Se invece usi un dominio personalizzato o Vercel/Netlify, lascia base: '/'
 
 export default defineConfig({
